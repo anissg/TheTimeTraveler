1b329532520b5087f9d7c494306bc4d67cebf04b
Amir BHK <amirbenhajkhaled@gmail.com>
2020-02-01 21:54:34 +0100

    MusicManager + mixers

---
+++ Assets/Scripts/Effects/FxPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FxPlayer : MonoBehaviour
+{
+    #region Public Properties
+    public AudioSource audioSource;
+    private bool play = false;
+
+    #endregion
+    // Awake is called when the script instance is being loaded
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Play(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.Play();
+        play = true;
+    }
+
+    private void Update()
+    {
+        if (!audioSource.isPlaying  && play)
+        { 
+            Destroy(gameObject);
+        }
+    }
+
+
+}
