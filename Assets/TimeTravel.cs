da4191acb5bc8e1b0b52e1dddd60238a6e1404ac
Anis HADJARI <anis_sg@hotmail.com>
2020-02-01 02:45:19 +0100

    - past, present transition effect

---
+++ Assets/TimeTravel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Era
+{
+    past,
+    present
+}
+
+public class TimeTravel : MonoBehaviour
+{
+    [SerializeField] private DigitalGlitch glitchEffect;
+    [SerializeField] private GameObject present;
+    [SerializeField] private GameObject past;
+
+    private Era era = Era.present;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        glitchEffect.intensity = Mathf.Max(0f, glitchEffect.intensity - 0.01f);
+        
+        if (Input.GetButtonUp("Fire1") && era == Era.present)
+        {
+            glitchEffect.intensity = 0.8f;
+            
+            present.SetActive(false);
+            past.SetActive(true);
+            era = Era.past;
+        }
+
+        if (Input.GetButtonUp("Fire2") && era == Era.past)
+        {
+            glitchEffect.intensity = 0.8f;
+
+            present.SetActive(true);
+            past.SetActive(false);
+            era = Era.present;
+        }
+    }
+}
