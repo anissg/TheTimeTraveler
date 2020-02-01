42df64f5876f5c21cbeecbef71ab64a822869261
Michel Chaker <michel.chaker@protonmail.com>
2020-02-01 20:50:44 +0200

    add lever that switches all scene

--- Assets/Scripts/GamePlay/Lever.cs
+++ Assets/Scripts/GamePlay/Lever.cs
@@ -1,17 +1,19 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Lever : MonoBehaviour {
     [SerializeField] private DigitalGlitch glitchEffect;
     [SerializeField] private GameObject present;
     [SerializeField] private GameObject past;
+    [SerializeField] private Sprite offTexture, onTexture;
 
     private Era era = Era.present;
     private bool playerInRange = false;
+    private bool leverState = false;
+    private SpriteRenderer _spriteRenderer;
 
-    void Start() { }
+    void Start() {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void Update() {
         glitchEffect.intensity = Mathf.Max(0f, glitchEffect.intensity - 0.01f);
@@ -19,6 +21,9 @@ public class Lever : MonoBehaviour {
         if (!playerInRange) return;
         
         if (Input.GetKeyDown(KeyCode.E)) {
+            leverState = !leverState;
+            _spriteRenderer.sprite = leverState ? onTexture : offTexture;
+            
             glitchEffect.intensity = 0.8f;
             present.SetActive(!present.activeSelf);
             past.SetActive(!past.activeSelf);
