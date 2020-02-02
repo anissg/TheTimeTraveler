9245c910bef25cc872e8425a6287820d4fd03eb4
Michel Chaker <michel.chaker@protonmail.com>
2020-02-02 04:17:22 +0200

    time fracture

---
+++ Assets/TimeParticle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TimeParticle : MonoBehaviour {
+    public GameObject timeFracture;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log(other.tag);
+        if (other.gameObject.CompareTag("Player")) {
+            Destroy(timeFracture);
+            Destroy(gameObject);
+        }
+    }
+}
