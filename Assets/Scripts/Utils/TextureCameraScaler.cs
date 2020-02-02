58139aee72e42a4e2c340818a17d809922bd8c43
Anis HADJARI <anis_sg@hotmail.com>
2020-02-02 03:55:51 +0100

    shader stuff

---
+++ Assets/Scripts/Utils/TextureCameraScaler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureCameraScaler : MonoBehaviour
+{ 
+    public void Start()
+    {
+        float y = Camera.main.orthographicSize * 2.0f;
+        float x = y * Screen.width / Screen.height;
+        transform.localScale = new Vector3(x, y, 1);
+    }
+}
